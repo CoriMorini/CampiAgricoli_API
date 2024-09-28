0e3dbfbbd1d330b83bc4906910d36c76663891fe
Nicolas Corini <nikycorini@gmail.com>
2024-09-28 22:31:28 +0200

    Modificata configurazione in modo che se avviati con profilo http siano visibili su rete locale. Aggiunto controller misurazioni con post list misurazioni da inserire in db.

---
+++ ApiEdilErreApp/Controllers/MisurazioniController.cs
@@ -0,0 +1,42 @@
+﻿using ApiCampiAgricoli.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.InMemory.Query.Internal;
+using System.Text.Encodings.Web;
+
+namespace ApiEdilErreApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Misurazioni : ControllerBase
+{
+
+
+
+    [HttpPost("PostMisurazione")]
+    public ActionResult UpdateMicrocontrollore([FromBody] List<TabMisurazioni> tmp)
+    {
+        using (var db = new CampiAgricoliContext())
+        {
+            try
+            {
+
+                foreach (TabMisurazioni item in tmp)
+                {
+                    item.dataOraCertaMisurazione = DateTime.Now;
+                    db.TabMisurazioni.Add(item);
+                }
+
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return NotFound(ex);
+            }
+            
+        }
+
+        return Ok();
+    }
+
+}
+    
